cf888c3dd6d98f1df40debbb70a501fc68cb0296
chemacenturion <centurion.philadelphia@gmail.com>
2021-07-08 18:28:56 -0400

    third readMe commit

--- Assets/script.js
+++ Assets/script.js
@@ -43,8 +43,8 @@ $(".saveBtn").on("click", function(e) {
 e.preventDefault();
 var value = $(this).siblings(".time-block").val()
 var time = $(this).parent().attr("id")
-// console.log(value)
-// console.log(time)
+//console.log(value)
+//console.log(time)
 localStorage.setItem(time, value)
 }
 )
